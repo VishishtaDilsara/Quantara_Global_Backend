6e61a75cdf099b7a7ce811d09c65454d9f69f4ea
“shanuDil2001” <shanukadilshan.swe@gmail.com>
2026-01-14 16:17:55 +0530

    Initialized a repository to develop Quantara Global Backend ✅

---
+++ index.js
@@ -0,0 +1,16 @@
+// Section 01: Import dependencies
+import express from "express";
+
+// Section 02: Create express app
+const app = express();
+
+// Section 03: Configure middleware
+
+// Section 05: Connenct with the MongoDB
+
+// Section 05: Define routes
+
+// Section 06: Start the server
+app.listen(3000, () => {
+  console.log("Surver is running!");
+});
